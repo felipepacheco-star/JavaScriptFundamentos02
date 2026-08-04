76aabf811239504efec75f50220443aeb66c9b16
FELIPE GONÇALVES PACHECO <26171611@edu119.local>
2026-08-04 14:08:37 -0300

    salvando alteração

--- lacos.js
+++ lacos.js
@@ -1,3 +1,66 @@
-for(let i =1; i<=5 i++){
-    console.log(i);
+// for(let i =1; i <=5; i++){
+//     console.log(i);
+// }
+
+// for(let i = 10; i >= 1; i--){
+//     console.log(i);
+// }
+
+//numeros pares
+// for(let i = 2; i <= 20; i += 2){
+//     console.log(i)
+// }
+
+// numeros impar
+// for(let i = 1; i <= 20; i += 2){
+//     console.log(i)
+// }
+
+//tabuada
+// for(let i = 1; i <= 10; i++){
+//     console.log(`5 X ${i} = ${5 * i}`)
+// }
+
+// for(let minuto = 0; minuto <= 59; minuto++){
+//     console.log(`minuto: ${minuto}`)
+// }
+
+// for(let hora = 0; hora <= 59; hora++){
+//     console.log(`hora: ${hora}`)
+// }
+
+// for(let hora = 0; hora <= 59; hora++){
+//     for(let minuto = 0; minuto <= 59; minuto++){
+//          console.log(`${hora}: ${minuto}`)
+//     }
+// }
+
+//pulando numero
+// for(let i = 1; i <= 10; i++){
+//     if( i == 5){
+//         continue;
+//     }
+//     console.log(i)
+// }
+
+//parando laço 
+// for(let i = 1; i <= 10; i++){
+//     if( i == 6){
+//         break;
+//     }
+//     console.log(i)
+// }
+
+// let soma = 0;
+
+// for(let i = 1; i <=10; i++){
+//     soma += i
+// }
+// console.log(`Soma fina e: ${soma}`)
+
+let numero = 1;
+
+while(numero <= 10){
+    console.log(numero);
+    numero++;
 }
